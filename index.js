64af712399e39f9f67418d70b8d66359c09a2b4e
gokulkrishna6 <goku@Kali.lan>
2024-12-30 02:58:10 +0530

    myblog

---
+++ index.js
@@ -0,0 +1,27 @@
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".nav-list ul");
+const menuItem = document.querySelectorAll(".nav-list ul li a");
+const header = document.querySelector(".header.container-main");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+});
+
+ 
+menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+  });
+});
+
+ 
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "black";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});

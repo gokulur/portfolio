65d69535d19a2bbf280cb42f5aed6a4ca3490bd9
gokulkrishna6 <goku@Kali.lan>
2024-12-30 22:36:09 +0530

    hero section updated

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ menuItem.forEach((item) => {
  
 document.addEventListener("scroll", () => {
   var scroll_position = window.scrollY;
-  if (scroll_position > 250) {
+  if (scroll_position > 10) {
     header.style.backgroundColor = "black";
   } else {
     header.style.backgroundColor = "transparent";
